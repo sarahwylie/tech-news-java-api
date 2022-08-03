0676103477af458caabe2d25a0244b523395dd25
sarahwylie <sarahannemariawylie@gmail.com>
2022-08-03 09:20:46 -0500

    fixed up the post error

--- src/main/java/com/technews/controller/TechNewsController.java
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -35,28 +35,31 @@ public class TechNewsController {
 
     @PostMapping("/users/login")
     public String login(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
-        if ((user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())){
-            model.addAttribute("notice", "Email address and password must be populated in order to log in");
+
+        if ((user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
+            model.addAttribute("notice", "Email address and password must be populated in order to login!");
             return "login";
         }
 
         User sessionUser = userRepository.findUserByEmail(user.getEmail());
 
         try {
-            // if sessionUser is invalid, running .equals() will throw an error
+            // If sessionUser is invalid, running .equals() will throw an error
             if (sessionUser.equals(null)) {
-            } //catch an error and notify user that email address is not recognized
+
+            }
+            // We will catch an error and notify client that email address is not recognized
         } catch (NullPointerException e) {
-            model.addAttribute("notice", "Email address is not recognized");
+            model.addAttribute("notice", "Email address is not recognized!");
             return "login";
         }
 
-        //validate password
+        // Validate Password
         String sessionUserPassword = sessionUser.getPassword();
         boolean isPasswordValid = BCrypt.checkpw(user.getPassword(), sessionUserPassword);
-        if (isPasswordValid = false){
-            model.addAttribute("notice", "Password is not valid");
-        return "login";
+        if(isPasswordValid == false) {
+            model.addAttribute("notice", "Password is not valid!");
+            return "login";
         }
 
         sessionUser.setLoggedIn(true);
@@ -67,24 +70,29 @@ public class TechNewsController {
 
     @PostMapping("/users")
     public String signup(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
+
         if ((user.getUsername().equals(null) || user.getUsername().isEmpty()) || (user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
-            model.addAttribute("notice", "To sign up, username, email, and password must be populated");
+            model.addAttribute("notice", "In order to signup username, email address and password must be populated!");
             return "login";
         }
+
         try {
-            //encrypt password
+            // Encrypt password
             user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
             userRepository.save(user);
         } catch (DataIntegrityViolationException e) {
-            model.addAttribute("notice", "Email address is not available. Please choose a different unique email address.");
+            model.addAttribute("notice", "Email address is not available! Please choose a different unique email address.");
             return "login";
         }
+
         User sessionUser = userRepository.findUserByEmail(user.getEmail());
+
         try {
             if (sessionUser.equals(null)) {
+
             }
         } catch (NullPointerException e) {
-            model.addAttribute("notice", "User is not recognized");
+            model.addAttribute("notice", "User is not recognized!");
             return "login";
         }
 
@@ -92,6 +100,24 @@ public class TechNewsController {
         request.getSession().setAttribute("SESSION_USER", sessionUser);
 
         return "redirect:/dashboard";
+    }
+
+    @PostMapping("/posts")
+    public String addPostDashboardPage(@ModelAttribute Post post, Model model, HttpServletRequest request) {
+
+        if ((post.getTitle().equals(null) || post.getTitle().isEmpty()) || (post.getPostUrl().equals(null) || post.getPostUrl().isEmpty())) {
+            return "redirect:/dashboardEmptyTitleAndLink";
+        }
+
+        if (request.getSession(false) == null) {
+            return "redirect:/login";
+        } else {
+            User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+            post.setUserId(sessionUser.getId());
+            postRepository.save(post);
+
+            return "redirect:/dashboard";
+        }
     }
 
     @PostMapping("/posts/{id}")
@@ -128,27 +154,32 @@ public class TechNewsController {
 
     @PostMapping("/comments/edit")
     public String createCommentEditPage(@ModelAttribute Comment comment, HttpServletRequest request) {
-        if (comment.getCommentText().isEmpty() || comment.getCommentText().equals(null)) {
-            return "redirect:/singlePostEmptyComment/" + comment.getPostId();
+
+        if (comment.getCommentText().equals("") || comment.getCommentText().equals(null)) {
+            return "redirect:/editPostEmptyComment/" + comment.getPostId();
         } else {
             if (request.getSession(false) != null) {
                 User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
                 comment.setUserId(sessionUser.getId());
                 commentRepository.save(comment);
-                return "redirect:/post/" + comment.getPostId();
+
+                return "redirect:/dashboard/edit/" + comment.getPostId();
             } else {
-                return "login";
+                return "redirect:/login";
             }
         }
+
     }
 
     @PutMapping("/posts/upvote")
     public void addVoteCommentsPage(@RequestBody Vote vote, HttpServletRequest request, HttpServletResponse response) {
+
         if (request.getSession(false) != null) {
             Post returnPost = null;
             User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
             vote.setUserId(sessionUser.getId());
             voteRepository.save(vote);
+
             returnPost = postRepository.getById(vote.getPostId());
             returnPost.setVoteCount(voteRepository.countVotesByPostId(vote.getPostId()));
         }
